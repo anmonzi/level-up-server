19da5465b48ab4ef87695d0f5bdf512d3ff563a8
Ashton Monzi <anmonzi@gmail.com>
2021-08-02 15:44:47 -0500

    initial commit

---
+++ levelupapi/models/game_type.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class GameType(models.Model):
+    """GameType model
+    fields:
+        label (CharField): name of the game type
+    """
+
+    label = models.CharField(max_length=50)
+    
