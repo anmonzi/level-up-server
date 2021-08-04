e47ae32c07d6f7a338e171913168da0f0f01c228
Ashton Monzi <anmonzi@gmail.com>
2021-08-04 14:02:54 -0500

    Ch.8 Done - added event views, serializers, and url path to server to getch all events for client

--- levelupapi/models/event.py
+++ levelupapi/models/event.py
@@ -4,7 +4,7 @@ from django.db import models
 class Event(models.Model):
     """Event Model
     Fields:
-        host (ForeignKey): the user that made the event
+        organizer (ForeignKey): the user that made the event
         game (ForeignKey): the game associated with the event
         date (DateField): The date of the event
         time (TimeFIeld): The time of the event
@@ -13,10 +13,10 @@ class Event(models.Model):
         attendees (ManyToManyField): The gamers attending the event
     """
 
-    host = models.ForeignKey("Gamer", on_delete=models.CASCADE)
+    organizer = models.ForeignKey("Gamer", on_delete=models.CASCADE)
+    description = models.TextField()
     game = models.ForeignKey("Game", on_delete=models.CASCADE)
     date = models.DateTimeField()
     time = models.TimeField()
-    description = models.TextField()
     title = models.CharField(max_length=100)
     attendees = models.ManyToManyField("Gamer", through="EventGamer", related_name="attending")
