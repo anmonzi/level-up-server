10a45c06c6bb6d90da39d1152af5f871c725c4f0
Ashton Monzi <anmonzi@gmail.com>
2021-08-04 12:00:43 -0500

    Ch.7 Done - views, serializer and urls added to get games from database

--- levelup/urls.py
+++ levelup/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf.urls import include
+from django.contrib import admin
 from django.urls import path
 from rest_framework import routers
 from levelupapi.views import register_user, login_user, GameTypeView, GameView
@@ -24,6 +25,7 @@ router.register(r'gametypes', GameTypeView, 'gametype')
 router.register(r'games', GameView, 'game')
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', include(router.urls)),
     path('register', register_user),
     path('login', login_user),
