423b051dcbbbcbc73b34bcedb46a1076a748c190
Ashton Monzi <anmonzi@gmail.com>
2021-08-09 13:32:27 -0500

    Ch.13 Done - added profile view/url path - client can receive profile information from server

--- levelup/urls.py
+++ levelup/urls.py
@@ -17,13 +17,14 @@ from django.conf.urls import include
 from django.contrib import admin
 from django.urls import path
 from rest_framework import routers
-from levelupapi.views import register_user, login_user, GameTypeView, GameView, EventView
+from levelupapi.views import register_user, login_user, GameTypeView, GameView, EventView, Profile
 
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'gametypes', GameTypeView, 'gametype')
 router.register(r'games', GameView, 'game')
 router.register(r'events', EventView, 'event')
+router.register(r'profile', Profile, 'profile')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
