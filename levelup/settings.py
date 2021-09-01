abc92d19c5da200e3362001ef8c41b4bb6064819
Ashton Monzi <anmonzi@gmail.com>
2021-09-01 16:39:09 -0500

    revised settings.py

--- levelup/settings.py
+++ levelup/settings.py
@@ -60,7 +60,7 @@ REST_FRAMEWORK = {
 CORS_ORIGIN_WHITELIST = (
     'http://localhost:3000',
     'http://127.0.0.1:3000',
-    'https://levelup-server.herokuapp.com/'
+    'https://levelup-server.herokuapp.com'
 )
 
 MIDDLEWARE = [
