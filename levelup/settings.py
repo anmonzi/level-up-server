30658a1f9d570fe1045eb92507b967be730afa1b
Ashton Monzi <anmonzi@gmail.com>
2021-09-02 11:53:51 -0500

    updated url in settings.py to client url not server

--- levelup/settings.py
+++ levelup/settings.py
@@ -58,9 +58,9 @@ REST_FRAMEWORK = {
 }
 
 CORS_ORIGIN_WHITELIST = (
-    # 'http://localhost:3000',
-    # 'http://127.0.0.1:3000',
-    'https://levelup-server.herokuapp.com',
+    'http://localhost:3000',
+    'http://127.0.0.1:3000',
+    'https://levelup-client.herokuapp.com',
 )
 
 MIDDLEWARE = [
